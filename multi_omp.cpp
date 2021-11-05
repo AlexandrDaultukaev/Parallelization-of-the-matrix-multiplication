066bf9589bef41c9a2e0d92d1c532aef90c0f82f
AlexandrDaultukaev <shyra31102000@mail.ru>
2021-11-05 14:54:57 +0700

    Add omp measurements

---
+++ multi_omp.cpp
@@ -0,0 +1,92 @@
+/* Умножение матриц с использованием OpenMP */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+#include <sys/time.h>
+#include <omp.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/times.h>
+#include <fcntl.h>
+#include <string.h>
+#include "CLI11.hpp"
+
+
+/* Выделение памяти под матрицы размером size*size */
+double *alloc_matrix(int size) {
+    double *matrix = (double*)malloc(size * size * sizeof(double));
+    printf("matrix %dx%d allocated\n", size, size);
+    return matrix;
+}
+
+/* Освобождение памяти матрицы */
+void del_matrix(double *matrix) {
+    free(matrix);
+}
+
+/* Функция для измерения времени работы */
+double dtime() {
+    struct timeval tv;
+    struct timezone tz;
+    double t;
+    gettimeofday(&tv, &tz);
+    t = (double)tv.tv_sec + (double)tv.tv_usec*1e-6;
+    return(t);
+}
+
+int main(int argc, char** argv) {
+    int N = 50; // Размер матрицы
+    int THR = 2; // Число потоков
+    int i, j, k;
+
+    CLI::App app{"App description"};
+    app.add_option("-n", N, "Size");
+    app.add_option("-t", THR, "Number of threads");
+    CLI11_PARSE(app, argc, argv);
+
+    /*
+       Устанавливаем, что среда выполнения
+       не может динамически изменять количество
+       потоков
+    */
+    omp_set_dynamic(0);
+
+    /* Задаем количество потоков */
+    omp_set_num_threads(THR);
+
+    /* Выделение памяти */
+    double *A = alloc_matrix(N);
+    double *B = alloc_matrix(N);
+    double *C = alloc_matrix(N);
+
+    srand(time(NULL));
+
+    /* Инициализация матриц значениями */
+    #pragma omp parallel for shared(A, B) private(i, j)
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            A[i + j * N] = rand() % 5;
+            B[i + j * N] = rand() % 5;
+        }
+    }
+
+    double t0 = dtime();
+    // shared - переменные A, B, C, N - общие для каждого потока
+    // private - переменные i, j, k - локальные для каждого потока
+    // omp for сам распределит итерации цикла между потоками
+    // барьерная синхронизация в for стоит по умолчанию
+    #pragma omp parallel for shared(A, B, C, N) private(i, j, k)
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            for (k = 0; k < N; k++) {
+                C[i + j * N] += A[i + k * N] * B[k + j * N];
+            }
+        }
+    }
+
+    printf("time for multiplying: %f\n", dtime()-t0);
+    return 0;
+}
---
+++ multi_pthread.cpp
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+#include <sys/time.h>
+#include <omp.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/times.h>
+#include <pthread.h>
+#include <fcntl.h>
+#include <string.h>
+#include "CLI11.hpp"
+
+int *alloc_matrix(int size){
+    int *matrix = (int*)malloc(size * size * sizeof(int));
+    printf("matrix %dx%d allocated\n", size, size);
+    return matrix;
+}
+
+void del_matrix(int *matrix){
+    free(matrix);
+}
+
+double dtime(){
+    struct timeval tv;
+    struct timezone tz;
+    double t;
+    gettimeofday(&tv, &tz);
+    t = (double)tv.tv_sec + (double)tv.tv_usec*1e-6;
+    return(t);
+}
+
+struct matrix_args{
+    int *m1;
+    int *m2;
+    int *ans;
+    int size;
+    int start;
+    int end;
+} ARG[100];
+
+void *multiply_matrix(void *pargs){
+    struct matrix_args *args = (struct matrix_args *) pargs;
+    int i, j, k, l, m, tmp = 0;
+    double t0 = dtime();
+    int *m1 = args->m1;
+    int *m2 = args->m2;
+    int *ans = args->ans;
+    int size = args->size;
+    int start = args->start;
+    int end = args->end;
+    for(i = start; i < end; i++){
+        m = i * size;
+        for(j = 0; j < size; j++){
+            l = 0;
+            for(k = 0; k < size; k++){
+                tmp += m1[m + k] * m2[j + l];
+                l += size;
+            }
+            ans[m + j] = tmp;
+            tmp = 0;
+        }
+    }
+    printf("thread works %fs\n", dtime() - t0);
+    pthread_exit(NULL);
+}
+
+int main(int argc, char** argv) {
+    int i, j, size, k, step, pos, res;
+    int THR_NUM,THR,N;
+
+    CLI::App app{"App description"};
+    app.add_option("-n", N, "Size");
+    app.add_option("-t", THR, "Number of threads");
+    CLI11_PARSE(app, argc, argv);
+
+    THR_NUM = THR;
+    size = N;
+
+    /* Создаем массив потоков */
+    pthread_t threads[THR_NUM];
+    pthread_attr_t attr;
+    printf("allocating\n");
+    int *m1 = alloc_matrix(size);
+    int *m2 = alloc_matrix(size);
+    int *ans = alloc_matrix(size);
+    srand(time(NULL));
+
+    for(int i=0; i<size; i++) {
+        for(int j=0; j<size; j++){
+            m1[i + j * size] = rand() % 5;
+            m2[i + j * size] = rand() % 5;
+        }
+    }
+
+    printf("multiplying\n");
+    double t0 = dtime();
+    /* Инициализация атрибутов потока */
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+    step = (int)((double)size/(double)THR_NUM);
+    pos = 0;
+    for(k = 0; k < THR_NUM; k++) {
+        ARG[k].m1 = m1;
+        ARG[k].m2 = m2;
+        ARG[k].ans = ans;
+        ARG[k].size = size;
+        ARG[k].start = pos;
+        pos += step;
+        ARG[k].end = (k == THR_NUM - 1) ? size : pos;
+        res = pthread_create(&threads[k], &attr, multiply_matrix, (void *)&ARG[k]);
+        if(res){
+            fprintf(stderr, "Error creating thread\n");
+            exit(1);
+        }
+    }
+    pthread_attr_destroy(&attr);
+    for(k = 0; k < THR_NUM; k++)
+        pthread_join(threads[k], NULL);
+    printf("time for multiplying: %f\n", dtime()-t0);
+    del_matrix(m1);
+    del_matrix(m2);
+    del_matrix(ans);
+    pthread_exit(NULL);
+    return 0;
+}
